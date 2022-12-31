83b887b96a21336b204d4471a47bec052d656ac9
Subbu <isubramanyam1995@gmail.com>
2022-12-31 21:47:10 +0530

    eureka added

--- src/main/java/com/dailycodebufferr/ProductService/service/ProductServiceImpl.java
+++ src/main/java/com/dailycodebufferr/ProductService/service/ProductServiceImpl.java
@@ -16,13 +16,14 @@ import static org.springframework.beans.BeanUtils.*;
 
 @Service
 @Log4j2
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
+
     @Override
     public Long addProduct(ProductRequest productRequest) {
         log.info("Adding Product ......");
-        Product product=Product.builder()
+        Product product = Product.builder()
                 .productName(productRequest.getName())
                 .price(productRequest.getPrice())
                 .quantity(productRequest.getQuantity())
@@ -34,12 +35,27 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public ProductResponse getProductById(long productId) {
-        log.info("Get the product for productId: {}",productId);
-       Product product= productRepository.findById(productId)
-               .orElseThrow(
-                       ()->new ProductServiceCustomException("Product with the given id not found","PRODUCT_NOT_FOUND"));
-       ProductResponse productResponse=new ProductResponse();
-        copyProperties(product,productResponse);
+        log.info("Get the product for productId: {}", productId);
+        Product product = productRepository.findById(productId)
+                .orElseThrow(
+                        () -> new ProductServiceCustomException("Product with the given id not found", "PRODUCT_NOT_FOUND"));
+        ProductResponse productResponse = new ProductResponse();
+        copyProperties(product, productResponse);
         return productResponse;
+    }
+
+    @Override
+    public void reduceQuantity(long productId, long quantity) {
+        log.info("Reduce Quantity {} for Id: {}", quantity, productId);
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ProductServiceCustomException("Product with given id not found", "PRODUCT_NOT_FOUND"));
+
+        if (product.getQuantity() < quantity) {
+            throw new ProductServiceCustomException("Product does not have sufficient Quantity", "INSUFFICIENT_QUANTITY");
+        }
+        product.setQuantity(product.getQuantity() - quantity);
+        productRepository.save(product);
+        log.info("Product Quantity reduced successfully");
     }
 }
